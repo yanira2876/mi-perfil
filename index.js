93fdc8ba2874fec84c427a3c213bd0b60c051f56
yanira2876 <162449857+yanira2876@users.noreply.github.com>
2024-04-16 10:57:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//declaración de SRING 
+
+let nombre="luisa";
+let segundonombre]="costa";
+let apellido="mora";
+let segundoapellido="rosas";
+console.log
+
+//declaracion de BOOLEAN
+
+let persona=true;
+let animal=false;
+let humano=true;
+let vegetal=false;
+console.log
---
+++ program.js
@@ -0,0 +1,17 @@
+let nombre= "pedro "
+let apellido= "mora"
+let altura=  1.53
+let edad= 75
+let persona= {nombre:"pedro",apellido:"mora"}
+let numeros= {altura:1.53,edad:75}
+let arreglo= ["pedro","mora"]
+let arreglouno= [1.53,75]
+
+let segundonombre = "luisa"
+let segundoapellido = "castro"
+let segundopeso= 90
+let segundoaño= 2022
+let personados= {nombre:"luisa",apellido:"castro"}
+let curso= {peso:90,altura:2022}
+let arreglodos= ["luisa","castro"]
+let arreglotres= [90,2022]
